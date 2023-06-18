a8c6b6545d5dda09e209032ee00fd02a5d62510d
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-06-18 12:37:40 +0200

    feat: add basic light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,32 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  daisyui: {
+    themes: [
+      {
+        'light': {
+          primary: '#4f46e5',
+          'primary-focus': '#4338ca',
+          'primary-content': '#ffffff',
+          secondary: '#6366f1',
+          'secondary-focus': '#4f46e5',
+          'secondary-content': '#ffffff',
+          'accent': '#a5b4fc',
+          'base': '#4b5563',
+          'base-100': '#ffffff',
+          'base-200': '#f3f4f6',
+          'base-300': '#374151',
+          'base-content': '#6b7280',
+          "neutral": "#111827"
+        },
+        // 'dark': {
+        //   'base-100': '#374151',
+        //   'base-200': '#1F2937',
+        //   'base-300': '#111827',
+        //   'base-content': '#6b7280'
+        // }
+      }
+    ]
   },
   plugins: [],
 }
