21a82dfff1a4d2bb6ca06cfb79f921907cb5c7a3
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-04 21:57:13 +0200

    wip: header - put Logo inside Link

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-    locales: ['en'],
-    defaultLocale: 'en'
+  locales: ['en'],
+  defaultLocale: 'en',
 });
 
 export const config = {
-    matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
+import Link from 'next-intl/link';
 import { Logo } from '@/src/components/Logo/Logo';
+import { Routes } from '@/src/utils/enums/routes';
 
 export const Header = () => {
   return (
     <header className="w-full ring-1 ring-gray-300">
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex items-center justify-between relative h-16">
         <div className="flex items-center">
-          <Logo className="text-neutral h-6" />
+          <Link href={Routes.Home}>
+            <Logo className="text-neutral h-6" />
+          </Link>
         </div>
         <div>{/*TODO: Buttons here*/}</div>
       </div>
