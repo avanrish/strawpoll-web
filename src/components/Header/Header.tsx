8121a08a3d7615609dc21a5fd4b0e2cf646911be
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-06 16:15:50 +0200

    wip: header - add burger icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next-intl/link';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 import { Logo } from '@/src/components/Logo/Logo';
 import { Routes } from '@/src/utils/enums/routes';
 
@@ -6,12 +7,14 @@ export const Header = () => {
   return (
     <header className="w-full ring-1 ring-gray-300">
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex items-center justify-between relative h-16">
-        <div className="flex items-center">
+        <div className="px-2">
           <Link href={Routes.Home}>
             <Logo className="text-neutral h-6" />
           </Link>
         </div>
-        <div>{/*TODO: Buttons here*/}</div>
+        <div className="p-2 rounded-md text-gray-400 hover:bg-base-200 transition-all">
+          <Bars3Icon className="h-6 w-6" />
+        </div>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    transitionDuration: {
+      DEFAULT: '250ms',
+    },
+  },
   daisyui: {
     themes: [
       {
