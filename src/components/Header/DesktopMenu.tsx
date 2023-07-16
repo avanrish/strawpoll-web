458c14020004c23c3b1148a0d87afc21cf611e1b
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-16 21:52:12 +0200

    feat: header with mobile and desktop menu

---
+++ src/components/Header/DesktopMenu.tsx
@@ -0,0 +1,40 @@
+import { getTranslations } from 'next-intl/server';
+import Link from 'next-intl/link';
+
+import { navLinks } from '@/src/utils/fixtures/navLinks';
+import { NavigationLink } from '@/src/components/Header/NavigationLink';
+
+export const DesktopMenu = async () => {
+  const t = await getTranslations('Header');
+  return (
+    <div className="hidden lg:flex lg:w-full ml-8 justify-between h-full">
+      <div className="flex space-x-8">
+        {navLinks.map(({ i18nKey, link, disabled }) => (
+          <NavigationLink
+            key={i18nKey}
+            link={link}
+            text={t(i18nKey)}
+            disabled={disabled}
+            isMobile={false}
+          />
+        ))}
+      </div>
+      <div className="flex items-center ml-auto text-sm space-x-3">
+        <Link
+          href="#"
+          className="font-medium px-4 aria-disabled:pointer-events-none aria-disabled:opacity-50"
+          aria-disabled
+        >
+          {t('login')}
+        </Link>
+        <Link
+          href="#"
+          className="btn btn-primary !text-sm !font-medium aria-disabled:pointer-events-none aria-disabled:opacity-50"
+          aria-disabled
+        >
+          {t('signUp')}
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,6 +10,7 @@ import pick from 'lodash/pick';
 import { Logo } from '@/src/components/Logo/Logo';
 import { Routes } from '@/src/utils/enums/routes';
 import { MobileMenu } from '@/src/components/Header/MobileMenu/MobileMenu';
+import { DesktopMenu } from '@/src/components/Header/DesktopMenu';
 
 export const Header = () => {
   const locale = useLocale();
@@ -17,8 +18,8 @@ export const Header = () => {
 
   return (
     <header className="w-full ring-1 ring-gray-300">
-      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex items-center justify-between relative h-16">
-        <div className="px-2">
+      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex items-center relative h-16">
+        <div className="px-2 lg:px-0">
           <Link href={Routes.Home}>
             <Logo className="text-neutral h-6" />
           </Link>
@@ -29,6 +30,7 @@ export const Header = () => {
         >
           <MobileMenu />
         </NextIntlClientProvider>
+        <DesktopMenu />
       </div>
     </header>
   );
