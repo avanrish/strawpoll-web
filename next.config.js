934b6a19148ce6ec18388c63728c27fde6130a4d
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-16 14:01:59 +0200

    chore: add node_env to distinguish production from development

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')(
-    // This is the default (also the `src` folder is supported out of the box)
-    './i18n.ts'
-);
+const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
-module.exports = withNextIntl({
-    // Other Next.js configuration ...
-});
+module.exports = withNextIntl({});
