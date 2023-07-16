ffcc5eb485562bae9de58b46deeaf2b866167cbf
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-16 21:50:51 +0200

    refactor: make NavigationLink more reusable

--- src/components/Header/NavigationLink.tsx
+++ src/components/Header/NavigationLink.tsx
@@ -1,26 +1,32 @@
-import { useTranslations } from 'next-intl';
 import Link from 'next-intl/link';
 
-import { NavLink } from '@/src/utils/fixtures/navLinks';
+import { INavigationLinkProps } from '@/src/components/Header/types';
 
 export const NavigationLink = ({
   link,
-  i18nKey,
+  text,
   icon: Icon,
   disabled,
-}: NavLink) => {
-  const t = useTranslations('Header');
-
+  isMobile,
+}: INavigationLinkProps) => {
   return (
     <Link
       href={link}
-      className="flex items-center space-x-3 p-3 rounded-md hover:bg-gray-50 aria-disabled:opacity-50 aria-disabled:pointer-events-none"
+      className={`navlink ${isMobile ? 'mobile' : 'desktop'}`}
       aria-disabled={disabled}
     >
-      <span className="w-6 h-6 text-accent">
-        <Icon />
+      {Icon ? (
+        <span className="w-6 h-6 text-accent">
+          <Icon />
+        </span>
+      ) : null}
+      <span
+        className={`text-neutral ${
+          isMobile ? '' : 'lg:text-base-300 lg:text-sm'
+        }`}
+      >
+        {text}
       </span>
-      <span className="text-neutral">{t(i18nKey)}</span>
     </Link>
   );
 };
--- src/components/Header/types.ts
+++ src/components/Header/types.ts
@@ -1,3 +1,13 @@
+import { Routes } from '@/src/utils/enums/routes';
+
 export interface IMobileUpperSectionProps {
   closeMenu: () => void;
+}
+
+export interface INavigationLinkProps {
+  link: Routes | '#';
+  text: string;
+  icon?: any;
+  disabled?: boolean;
+  isMobile: boolean;
 }
