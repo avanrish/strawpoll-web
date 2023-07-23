763baac2821d9a49a41c1e384ea826e263fd0380
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-23 12:23:02 +0200

    refactor: remove Random component from index page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,3 @@
-import { useTranslations } from 'next-intl';
-import { Random } from '@/src/components/Random/Random';
-
 export default function Home() {
-  const t = useTranslations('Index');
-
-  return (
-    <div>
-      <Random />
-    </div>
-  );
+  return <div>Hello</div>;
 }
