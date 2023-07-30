55ba0728694bb121f5bb22196e0910edc790911f
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-07-30 14:30:05 +0200

    feat: add sun and moon icons to ThemeSwitch

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { Switch } from '@headlessui/react';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 
 import { Themes } from '@/src/utils/enums/themes';
 import { axiosNext } from '@/src/utils/axios';
@@ -26,7 +27,13 @@ export const ThemeSwitch = ({ defaultEnabled }: ThemeSwitchProps) => {
       onChange={handleChange}
       className="relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-indigo-600 ui-not-checked:bg-gray-400"
     >
-      <span className="inline-block h-4 w-4 transform rounded-full bg-white transition ui-checked:translate-x-6 ui-not-checked:translate-x-1" />
+      <span className="inline-flex items-center justify-center h-4 w-4 transform rounded-full bg-white transition ui-checked:translate-x-6 ui-not-checked:translate-x-1">
+        {enabled ? (
+          <MoonIcon className="h-3 w-3 text-gray-900" />
+        ) : (
+          <SunIcon className="h-3 w-3 text-yellow-600" />
+        )}
+      </span>
     </Switch>
   );
 };
