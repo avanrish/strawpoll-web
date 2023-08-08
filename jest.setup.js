2c044c6dead565da4aea0238958839fbd0bf38dd
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-08-08 19:57:50 +0200

    chore: add environmental variables to tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,12 @@
-// Polyfill "window.fetch" used in the React component.
 import 'whatwg-fetch';
-
-// Extend Jest "expect" functionality with Testing Library assertions.
 import '@testing-library/jest-dom';
 
 import { server } from './src/mocks/server';
+
+process.env = Object.assign(process.env, {
+  NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
+  NEXT_PUBLIC_API_URL: 'http://localhost:4000/api',
+});
 
 beforeAll(() => {
   server.listen();
