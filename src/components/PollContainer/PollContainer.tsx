1c720514a5f8065371cafb81d247b9722aef5cb6
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-08-27 16:48:27 +0200

    refactor: add optional property that removes margin to PollContainer

--- src/components/PollContainer/PollContainer.tsx
+++ src/components/PollContainer/PollContainer.tsx
@@ -1,8 +1,15 @@
-import { PropsWithChildren } from 'react';
+import { PollContainerProps } from './PollContainer.type';
 
-export const PollContainer = ({ children }: PropsWithChildren) => {
+export const PollContainer = ({
+  withoutMargin,
+  children,
+}: PollContainerProps) => {
   return (
-    <div className="bg-white dark:bg-gray-800 mt-8 border max-sm:border-x-0 border-t-4 border-t-amber-400 dark:border-t-indigo-400 border-b-gray-300 dark:border-gray-800 py-5 px-4 sm:p-6 sm:rounded-md">
+    <div
+      className={`bg-white dark:bg-gray-800 border max-sm:border-x-0 border-t-4 border-t-amber-400 dark:border-t-indigo-400 border-b-gray-300 dark:border-gray-800 py-5 px-4 sm:p-6 sm:rounded-md ${
+        withoutMargin ? '' : 'mt-8'
+      }`}
+    >
       {children}
     </div>
   );
