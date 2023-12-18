1f0ebfc63e39ea40ebccf58c083ec8347d7b489c
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-12-18 21:50:33 +0100

    chore(deps): remove whatwg-fetch and msw

--- jest.setup.js
+++ jest.setup.js
@@ -1,21 +1,6 @@
-import 'whatwg-fetch';
 import '@testing-library/jest-dom';
-
-import { server } from './src/mocks/server';
 
 process.env = Object.assign(process.env, {
   NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
   NEXT_PUBLIC_API_URL: 'http://localhost:4000/api',
-});
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
 });
